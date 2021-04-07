84d091f5400ccd526ccf130ed7a648f1fe79aec9
Your Name <suellenwmj@hotmail.com>
2021-04-06 21:40:48 -0300

    Criação da função adicionar filmes

---
+++ js/funcoes.js
@@ -0,0 +1,11 @@
+function AdicionarFilme(){
+    let campoFilme = document.querySelector("#filme");
+//console.log("campoFilme");
+let imagemFilme = campoFilme.value;
+//console.log(imagemFilme);
+ListarFilmesNaTela();
+}
+
+function ListarFilmesNaTela(){
+    console.log("ola");
+}
